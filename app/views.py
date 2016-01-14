3ece0683e1b3427ce6de0491001d86c78babd506
Victor Moraes <vdmoraes94@gmail.com>
2016-01-14 11:17:07 -0200

    Finalizando formulário de login

--- app/views.py
+++ app/views.py
@@ -6,24 +6,24 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-def index():
-	user = {'nickname' : 'Victor'}
-	posts = [
-		{
-			'author': {'nickname': 'Felipe'},
-			'body': 'O trabalho no McDonalds esta muito bom!'
-		},
-		{
-			'author': {'nickname': 'Pedro'},
-			'body': 'Respeitem minha historia nas brigas de rua!'
-		},
-		{
-			'author': {'nickname': 'Matheus Martins'},
-			'body': 'Alguem disse anime?'
-		}
-	]
+# def index():
+# 	user = {'nickname' : 'Victor'}
+# 	posts = [
+# 		{
+# 			'author': {'nickname': 'Felipe'},
+# 			'body': 'O trabalho no McDonalds esta muito bom!'
+# 		},
+# 		{
+# 			'author': {'nickname': 'Pedro'},
+# 			'body': 'Respeitem minha historia nas brigas de rua!'
+# 		},
+# 		{
+# 			'author': {'nickname': 'Matheus Martins'},
+# 			'body': 'Alguem disse anime?'
+# 		}
+# 	]
 
-	return render_template('index.html', user=user, title='Vapo', posts=posts)
+# 	return render_template('index.html', user=user, title='Vapo', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -31,4 +31,4 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for OpenID="%s", remember_me=%s' %(form.openid.data, str(form.remember_me.data)))
 		return redirect('/index')
-	return render_template('login.html', title='Sign In', form=form)	
+	return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])	
