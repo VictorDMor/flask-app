3942809ed576e1c8518366008a15c36f0e7f0534
Victor Moraes <vdmoraes94@gmail.com>
2016-01-14 10:55:52 -0200

    Adição de login para a view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 #encoding:utf-8
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -23,3 +24,8 @@ def index():
 	]
 
 	return render_template('index.html', user=user, title='Vapo', posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)	
