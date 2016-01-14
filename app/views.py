434fe7ce2961cb7e102d65d3fdfa46d169510516
Victor Moraes <vdmoraes94@gmail.com>
2016-01-14 11:18:35 -0200

    Finalizando formulário de login e arrumando index

--- app/views.py
+++ app/views.py
@@ -6,24 +6,24 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-# def index():
-# 	user = {'nickname' : 'Victor'}
-# 	posts = [
-# 		{
-# 			'author': {'nickname': 'Felipe'},
-# 			'body': 'O trabalho no McDonalds esta muito bom!'
-# 		},
-# 		{
-# 			'author': {'nickname': 'Pedro'},
-# 			'body': 'Respeitem minha historia nas brigas de rua!'
-# 		},
-# 		{
-# 			'author': {'nickname': 'Matheus Martins'},
-# 			'body': 'Alguem disse anime?'
-# 		}
-# 	]
+def index():
+	user = {'nickname' : 'Victor'}
+	posts = [
+		{
+			'author': {'nickname': 'Felipe'},
+			'body': 'O trabalho no McDonalds esta muito bom!'
+		},
+		{
+			'author': {'nickname': 'Pedro'},
+			'body': 'Respeitem minha historia nas brigas de rua!'
+		},
+		{
+			'author': {'nickname': 'Matheus Martins'},
+			'body': 'Alguem disse anime?'
+		}
+	]
 
-# 	return render_template('index.html', user=user, title='Vapo', posts=posts)
+	return render_template('index.html', user=user, title='Vapo', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
