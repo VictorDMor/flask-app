a3e57070357270ec355bea96fe6265b73af4f310
Victor Moraes <vdmoraes94@gmail.com>
2016-01-15 09:37:40 -0200

    Tirei o login via OpenID, muito trabalhoso e obsoleto também

--- app/models.py
+++ app/models.py
@@ -9,28 +9,6 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
 
-	@property
-	def is_authenticated(self):
-	    return True
-
-	@property
-	def is_active(self):
-	    return True
-
-	@property
-	def is_anonymous(self):
-		return False
-	
-	def get_id(self):
-		try:
-			return unicode(self.id)
-		except NameError:
-			return str(self.id)
-
-	def __repr__(self):
-		return '<User %r>' % (self.nickname)
-	
-
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(140))
