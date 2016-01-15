d3e30a53c0c48047e8517622bd05ed86779c9766
Victor Moraes <vdmoraes94@gmail.com>
2016-01-15 08:58:09 -0200

    Carregamento de usuário

--- app/views.py
+++ app/views.py
@@ -4,6 +4,10 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
 @app.route('/')
 @app.route('/index')
 def index():
