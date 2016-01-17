6c72900cb622e19545ec6c3549bf3eb2f22a0740
Victor Moraes <vdmoraes94@gmail.com>
2016-01-17 10:01:52 -0200

    Ferramenta de postagem 100% operacional

--- app/views.py
+++ app/views.py
@@ -3,32 +3,25 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, db
 from .models import User
+import pdb
 
 def load_user(id):
 	return User.query.get(int(id))
 
+posts = []
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+
 	user = {'nickname': 'Victor'}
-	posts = [
-		{
-			'author': {'nickname': 'Felipe'},
-			'body': 'O trabalho no McDonalds esta muito bom!'
-		},
-		{
-			'author': {'nickname': 'Pedro'},
-			'body': 'Respeitem minha historia nas brigas de rua!'
-		},
-		{
-			'author': {'nickname': 'Matheus Martins'},
-			'body': 'Alguem disse anime?'
-		}
-	]
-	return render_template('index.html', title='Novo post adicionado!', user=user, posts=posts)
-	# posts.append({
-	# 	'author': {'nickname': request.form['name']},
-	# 	'body': request.form['post']
-	# })
+	if len(request.args) != 0:
+		posts.append({
+	 		'author': {'nickname': request.args['name']},
+			'body': request.args['post']
+		})
+		return render_template('index.html', title='Novo post adicionado!', user=user, posts=posts)
+	else:
+		return render_template('index.html', title='Novo post adicionado!', user=user, posts=posts)
+
 
 
