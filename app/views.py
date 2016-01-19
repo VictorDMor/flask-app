782821081af5524fe68c837db839c6dadff70c00
Victor Moraes <vdmoraes94@gmail.com>
2016-01-19 12:46:39 -0200

    Iniciando integração com BD

--- app/views.py
+++ app/views.py
@@ -1,9 +1,8 @@
 #encoding:utf-8
 
 from flask import render_template, flash, redirect, session, url_for, request, g
-from app import app, db
-from .models import User
-import pdb
+from app import app, db, models
+from models import User
 
 def load_user(id):
 	return User.query.get(int(id))
@@ -12,7 +11,6 @@ posts = []
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-
 	user = {'nickname': 'Victor'}
 	if len(request.args) != 0:
 		posts.append({
@@ -23,5 +21,15 @@ def index():
 	else:
 		return render_template('index.html', title='Novo post adicionado!', user=user, posts=posts)
 
-
+@app.route('/signup', methods=['GET', 'POST'])
+def sign():
+	if len(request.form) != 0:
+		u = models.User(nickname=request.form['nickname'], email=request.form['email'], pw=request.form['pword'])
+		db.session.add(u)
+		db.session.commit()
+		flash('Usuário cadastrado com sucesso!')
+		return render_template('signup.html', title='Cadastro de usuario')
+	else:
+		print 'não entrou'
+		return render_template('signup.html', title='Cadastro de usuario')
 
