e9190cdc1d44e64fbe10e1ff8ef77ca8864eff53
Victor Moraes <vdmoraes94@gmail.com>
2016-01-20 12:16:38 -0200

    Comecei o Login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
+#encoding: utf-8
+
 from app import db
+from flask.ext.login import LoginManager
+lm = LoginManager()
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -6,9 +10,26 @@ class User(db.Model):
 	email = db.Column(db.String(120), index=True, unique=True)
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	pw = db.Column(db.String(64), index=True, unique=True)
+	authenticated = db.Column(db.Boolean, index=True, unique=True)
+
+	def is_active(self):
+		return True
+
+	def get_id(self):
+		''' Retorna o e-mail do usuario '''
+		return self.email
+
+	def is_authenticated(self):
+		''' True se o usuário estiver autenticado '''
+		return self.authenticated
 
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
+
+	@lm.user_loader
+	def user_loader(user_id):
+		''' Dado o id do usuário, retornar o objeto Usuário correspondente '''
+		return User.query.get(user_id)
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
--- app/views.py
+++ app/views.py
@@ -2,7 +2,11 @@
 
 from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, db, models
+from flask.ext.wtf import Form
+from .forms import LoginForm
 from models import User
+from flask.ext.login import LoginManager
+lm = LoginManager()
 
 def load_user(id):
 	return User.query.get(int(id))
@@ -24,11 +28,26 @@ def index():
 @app.route('/signup', methods=['GET', 'POST'])
 def sign():
 	if len(request.form) != 0:
-		u = models.User(nickname=request.form['nickname'], email=request.form['email'], pw=request.form['pword'])
+		u = models.User(nickname=request.form['nickname'], email=request.form['email'], pw=request.form['pword'], authenticated=False)
 		db.session.add(u)
 		db.session.commit()
 		flash('Usuario cadastrado com sucesso!')
 		return render_template('signup.html', title='Cadastro de usuario')
 	else:
 		return render_template('signup.html', title='Cadastro de usuario')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.get(form.email.data)
+		if user:
+			if bcrypt.check_password_hash(user.pw, form.pword.data):
+				user.authenticated = True
+				db.session.add(user)
+				db.session.commit()
+				login_user(user, remember=True)
+				return redirect(url_for('index'))
+ 	return render_template('login.html', title='Login de usuario')
+ 		
 
